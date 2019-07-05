6ee0c8cc608feb61ab3904fc643481c06d226f78
黄敬仁 <hjingren@aliyun.com>
2019-07-05 13:57:19 +0800

    :refactor Refactor code

---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react';
+import styles from './index.scss'
+
+export default class index extends PureComponent {
+  render() {
+    return (
+      <div className={styles.homeBox}>
+        webpack-template
+      </div>
+    )
+  }
+}
