3a024faddf6edc0b1bdfb087841111a6aab17b36
hzzly <hjingren@aliyun.com>
2021-03-05 11:28:43 +0800

    fix: 优化

--- src/services/home.ts
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: hzzly
- * @Date: 2021-03-04 10:19:41
- * @LastEditors: hzzly
- * @LastEditTime: 2021-03-04 10:19:41
- * @Copyright: hzzly(hjingren@aliyun.com)
- * @Description: description
- */
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,26 +1,29 @@
-// import { fetch } from '@youpai/common';
+import { fetch } from '@youpai/common';
 
-// // const delay = (ms) =>
-// //   new Promise((resolve) => {
-// //     setTimeout(resolve, ms);
-// //   });
-
-// /**
-//  * get
-//  * @param {*} params
-//  */
-// export async function get(params: any) {
-//   return fetch({ url: `/xxxxxxx.html?${stringify(params)}` });
-// }
-
-// /**
-//  * post
-//  * @param {*} params
-//  */
-// export async function post(params: any) {
-//   return fetch({
-//     url: `/xxxxxxx.html`,
-//     method: 'post',
-//     data: params,
+// const delay = (ms) =>
+//   new Promise((resolve) => {
+//     setTimeout(resolve, ms);
 //   });
-// }
+
+/**
+ * get 请求
+ * @param {*} params
+ */
+export async function get(params: any) {
+  return fetch({
+    url: `/xxxxxxx.html`,
+    data: params,
+  });
+}
+
+/**
+ * post 请求
+ * @param {*} params
+ */
+export async function post(params: any) {
+  return fetch({
+    url: `/xxxxxxx.html`,
+    method: 'post',
+    data: params,
+  });
+}
